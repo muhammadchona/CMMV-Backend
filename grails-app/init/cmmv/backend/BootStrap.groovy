76783606c830c26b01217ae5248e30201d7cc291
Joao Mbjaia <joao.mbjaia@fgh.org.mz>
2021-12-15 14:06:37 +0200

    Login por Mobilizer Part_1

--- grails-app/init/cmmv/backend/BootStrap.groovy
+++ grails-app/init/cmmv/backend/BootStrap.groovy
@@ -6,11 +6,16 @@ import grails.plugin.springsecurity.SecurityFilterPosition
 import grails.plugin.springsecurity.SpringSecurityUtils
 import groovy.sql.Sql
 import com.twilio.Twilio
+import mz.org.fgh.cmmv.backend.clinic.Clinic
+import mz.org.fgh.cmmv.backend.distribuicaoAdministrativa.Province
 import mz.org.fgh.cmmv.backend.messages.TwilioDetail
-import mz.org.fgh.cmmv.backend.messages.TwilioDetailService;
+import mz.org.fgh.cmmv.backend.messages.TwilioDetailService
+import mz.org.fgh.cmmv.backend.mobilizer.CommunityMobilizer;
 import mz.org.fgh.cmmv.backend.protection.SecRole
 import mz.org.fgh.cmmv.backend.protection.SecUser
 import mz.org.fgh.cmmv.backend.protection.SecUserSecRole
+import mz.org.fgh.cmmv.backend.userLogin.MobilizerLogin
+import mz.org.fgh.cmmv.backend.userLogin.UserLogin
 
 import javax.persistence.EntityNotFoundException
 
@@ -41,8 +46,41 @@ class BootStrap {
         def testUser = new SecUser('user', 'password','user').save()
         def adminUser = new SecUser('admin', 'admin','admin').save()
 
+        def totalReg = CommunityMobilizer.count()
+        def comMobilizer
+        def mobLogin = MobilizerLogin.findByUsername('admin1')
+        if (totalReg == 1) {
+            comMobilizer = new CommunityMobilizer(firstNames: 'Adao', lastNames: 'Geneses', cellNumber: '840000000', cellNumber2: '860000000', uuid: '123456789-123456-asdqeasd-4sdase', clinic: Clinic.first()).save()
+
+            if(!mobLogin){
+
+                mobLogin = new MobilizerLogin()
+                mobLogin.username = 'admin1'
+                mobLogin.password = 'admin1'
+                mobLogin.fullName = 'Adao Geneses'
+                mobLogin.mobilizer = comMobilizer
+                mobLogin.province = Province.first()
+                mobLogin.district = mobLogin.province.districts.first()
+
+                mobilizerLoginService.save(mobLogin)
+            }
+
+        }
+
+        def clinicUser = new UserLogin()
+        clinicUser.username = 'clinic'
+        clinicUser.password = 'clinic'
+        clinicUser.fullName = 'Clinic_Starter'
+        clinicUser.clinic = Clinic.first()
+
+        clinicUser.save()
+
+
         SecUserSecRole.create testUser, userRole
         SecUserSecRole.create adminUser, adminRole
+
+        SecUserSecRole.create mobLogin as SecUser, adminRole
+        SecUserSecRole.create clinicUser as SecUser, adminRole
 
         SecUserSecRole.withSession {
             it.flush()
